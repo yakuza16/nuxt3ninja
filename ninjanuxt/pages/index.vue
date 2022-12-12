36f334c6774c5b1bdeb14ce6e091cc0b8b62e425
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2022-12-12 20:43:20 +0100

    .

---
+++ ninjanuxt/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, error
+      est eos ex consectetur, beatae dolore exercitationem quibusdam soluta,
+      debitis inventore saepe rerum aliquid tempore possimus repudiandae nemo id
+      eaque culpa expedita? Veniam, a. Voluptas illo officiis ab tenetur velit
+      at aperiam voluptatibus beatae unde, libero placeat repellat quasi a!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
