2533356776b9fb5b7f7ce14e947a21c5aba8c447
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2022-12-13 19:58:24 +0100

    routes

---
+++ ninjanuxt/pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eveniet
+      fugiat blanditiis, nostrum tenetur doloremque saepe optio ex id sint.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
